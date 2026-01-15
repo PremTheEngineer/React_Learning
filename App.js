c347dfed373d298d5bc7477d7bed13aa7785c79a
Prem Bhatt <prem.bhatt@unifyapps.com>
2026-01-16 01:32:50 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+// Elements banata hai react and root banata hai react dom
+// In the { } we give attributes such as id, class etc
+// multiple siblings can be added via an array
+// React element is nothing but an object
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement(
+      "h1",
+      {},
+      "I am a H1 inside div, thats inside another div"
+    ),
+    React.createElement("p", {}, "This is a paragraph sibling to above h1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement(
+      "h1",
+      {},
+      "I am another H1 inside div, thats inside another div"
+    ),
+    React.createElement("p", {}, "This is a paragraph sibling to above h1"),
+  ]),
+]);
+// But this syntax can get very cluttered so we write jsx
+const heading = React.createElement("h1", {}, "Hello world from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ script.js
@@ -0,0 +1,4 @@
+const heading = document.createElement("h1");
+heading.innerHTML = "This was created using JS only 👌🏻";
+const root = document.getElementById("root");
+root.appendChild(heading);
